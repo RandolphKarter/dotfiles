91138f8909cfda6ee0f9b60bd90f755ecbeb53d7
RandolphKarter <f.mymind777@gmail.com>
2023-11-05 18:54:22 +0300

    update v0.1

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,3 +1,18 @@
+#! /bin/sh
+
 killall -q polybar
 echo "---" | tee -a /tmp/mybar.log
-polybar mybar >> /tmp/mybar.log
+
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: Core 0" ]; then
+        export HWMON_PATH="$i"
+        echo ${HWMON_PATH} | tee -a /tmp/mybar.log
+    fi
+done
+
+polybar primary >> /tmp/mybar.log &
+
+connected=$(xrandr | grep 'HDMI-1 connected')
+if [ "${connected}" ]; then
+    polybar secondary >> /tmp/mybar.log &
+fi
